9bdfa58c847f04f457aec23d6830ceba00e1a19c
20wh1a6649 <93580048+20wh1a6649@users.noreply.github.com>
2021-12-01 11:28:04 +0530

    Create Stacks_arrays.c

---
+++ Week 3/Stacks_arrays.c
@@ -0,0 +1,74 @@
+
+
+
+/*
+OUTPUT:
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 1
+Enter element to be pushed : 8
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 1
+Enter element to be pushed : 3
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 1
+Enter element to be pushed : 2
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 1
+Enter element to be pushed : 3
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 1
+Enter element to be pushed : 5
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 4
+5
+ 3
+ 2
+ 3
+ 8
+ 1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 2
+Deleted element is 5
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 3
+Top element is 3
+1 : push
+2 : pop
+3 : peek
+4 : display
+5 : exit
+Enter your choice : 5
+
+
+*/
